a119f5c7b5fe529b9de44accbeba4d9cfb604e99
Adex Trade Platforms <adextradeplatforms@gmail.com>
2025-12-22 10:19:42 +0100

    Fix Netlify routing

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://adex-trade-backend-testing.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
+// attach token
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
